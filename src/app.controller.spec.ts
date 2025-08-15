1f0bd0ab9ce2fdc3e50b3b8e19e6585ea9e6e499
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-08-15 14:59:03 -0300

    test: update passed condition and importations

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getStatus()).toBe('Hello World!');
+    it('should return "Server is running..."', () => {
+      expect(appController.getStatus()).toBe('Server is running...');
     });
   });
 });
