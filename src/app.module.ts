f9291da5eed33890625f1ddb40d138f9edc88432
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-08-15 12:57:14 -0300

    feat: add typeorm connection and class validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+      ssl: { rejectUnauthorized: false },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
